440cbd68102bac1786d80bfc7e9db42b0d4538d6
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2022-06-13 19:04:44 +0900

    edit

---
+++ javascript/about.js
@@ -0,0 +1,22 @@
+//メニューの表示・非表示
+$(".menubtn").click(function(){
+    if($("#menu").hasClass("hidden")){
+        $("#menu").removeClass("hidden");
+        $(".line").css("background-color","white");
+    }else{
+        $("#menu").addClass("hidden");
+        $(".line").css("background-color","black");
+    }
+    $(this).toggleClass("active");
+});
+
+//メニューの非表示
+$("#menu").click(function(){
+    $("#menu").addClass("hidden");
+    $(".line").css("background-color","black");
+    $(".menubtn").toggleClass("active");
+});
+
+$("#title").click(function(){
+    window.location.href = "../index.html";
+});
